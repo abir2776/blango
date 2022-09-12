643cdbf31d38cfa17bd0d67f596fa9255d30cde1
MD. GONI <osman15-2776@diu.edu.bd>
2022-09-12 18:15:51 +0000

    Finish crispy forms

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from crispy_forms.layout import Submit
+from crispy_forms.helper import FormHelper
+from .models import Comment
+from django import forms
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
+
